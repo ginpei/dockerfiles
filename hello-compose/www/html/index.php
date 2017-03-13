bcd8c7d3dde64efdccf28e334312b968af3235f4
ginpei <ginpei@gmail.com>
2017-03-13 00:37:17 -0700

    mysql

--- hello-compose/www/html/index.php
+++ hello-compose/www/html/index.php
@@ -6,6 +6,33 @@
 	</head>
 	<body>
 		<h1>Hello World!</h1>
-<?php phpinfo(); ?>
+<?
+// $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+
+$host = 'mysql';
+$dbname = 'project';
+$user = 'project';
+$password = 'project';
+$connection = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $user, $password);
+
+// $query = 'select table_name from information_schema.tables'
+// 	. ' where table_type="BASE TABLE"';
+$query = 'select message from messages';
+?>
+	<p>Query: <code><?= $query ?></code></p>
+<?
+$stat = $connection->query($query);
+$items = $stat->fetchAll(PDO::FETCH_COLUMN);
+
+if (empty($items)) { ?>
+	<p>There are no tables!</p>
+<? } else { ?>
+	<ul>
+		<? foreach($items as $item) { ?>
+			<li><?= $item ?></li>
+		<? } ?>
+	</ul>
+<? }
+?>
 	</body>
 </html>
