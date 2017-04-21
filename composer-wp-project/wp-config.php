8fc92b0c65cfdac1e5c640c6521a5e52aea0b18d
ginpei <ginpei@gmail.com>
2017-04-21 13:08:40 -0700

    make it general name

--- composer-wp-project/wp-config.php
+++ composer-wp-project/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'myuser');
 define('DB_PASSWORD', 'mypassword');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql');
+define('DB_HOST', 'db');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
